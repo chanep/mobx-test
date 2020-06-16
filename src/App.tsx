d834d9d04c15e93bf5679cb2fc2f439351b12c5c
Esteban Canepa <esteban.canepa@rga.com>
2020-06-16 19:00:18 -0300

    UserStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useContext } from 'react';
 import './App.css';
+import { UserStore } from './UserStore';
+import { useObserver } from 'mobx-react-lite';
+
+const userContext = React.createContext(new UserStore());
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const userStore = useContext(userContext) as UserStore;
+
+  console.log("render");
+
+  return useObserver(() =>(
+      <div className="App">
+        <button onClick={() => userStore.getAll()}>Get All</button>
+        <p>Cantidad {userStore.users.length}</p>
+        <p>Status {userStore.status}</p>
+      </div>
+    )
   );
+
 }
 
 export default App;
